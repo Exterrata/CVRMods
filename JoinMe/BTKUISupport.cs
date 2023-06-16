d36f57d2c2d2c3b010b3cf660291ac8322f25f46
Exterrata <38149279+Exterrata@users.noreply.github.com>
2023-06-16 11:09:17 -0400

    upload files

---
+++ JoinMe/BTKUISupport.cs
@@ -0,0 +1,12 @@
+﻿using BTKUILib;
+
+namespace Koneko;
+internal class BTKUISupport
+{
+    public static void Initialize()
+    {
+        var misc = QuickMenuAPI.MiscTabPage;
+        var mainCatagory = misc.AddCategory("JoinMe");
+        mainCatagory.AddToggle("Enabled", "Enable JoinMe", JoinMe.Enabled.Value).OnValueUpdated += b => JoinMe.Enabled.Value = b;
+    }
+}
