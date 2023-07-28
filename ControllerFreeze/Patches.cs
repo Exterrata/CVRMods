c7b53dac9c2a9b58f9347ee8a5414abaf7133c83
Exterrata <38149279+Exterrata@users.noreply.github.com>
2023-07-28 13:00:45 -0400

    rewrite ControllerFreeze and remove unused icons

--- ControllerFreeze/Patches.cs
+++ ControllerFreeze/Patches.cs
@@ -1,19 +1,47 @@
-﻿using ABI_RC.Core.Player;
-using ABI_RC.Core.Savior;
-using ABI_RC.Systems.IK.SubSystems;
-using HarmonyLib;
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Koneko;
 internal class Patches
 {
+    public static Dictionary<int, Controller> controllers;
+
+    public class Controller
+    {
+        public Transform transform;
+        public Vector3 PrevPos;
+        public Quaternion PrevRot;
+        public Vector3 FreezePos;
+        public Quaternion FreezeRot;
+        public bool frozen;
+    }
+
     [HarmonyPostfix]
-	[HarmonyPatch(typeof(PlayerSetup), "Update")]
-	public static void KnucklesAlwaysTrack()
-	{
-	    if (MetaPort.Instance.isUsingVr)
-	    {
-	        BodySystem.TrackingLeftArmEnabled = true;
-	        BodySystem.TrackingRightArmEnabled = true;
-	    }
-	}
+    [HarmonyPatch(typeof(Valve.VR.SteamVR_Behaviour_Pose), "SteamVR_Behaviour_Pose_OnUpdate")]
+    public static void FreezeControllers(Transform ___origin, int ___deviceIndex)
+    {
+        if (!controllers.ContainsKey(___deviceIndex)) {
+            var controller = new Controller();
+            controller.transform = ___origin;
+            controllers.Add(___deviceIndex, controller);
+        }
+
+        if (controllers[___deviceIndex].transform.position != controllers[___deviceIndex].PrevPos)
+        {
+            controllers[___deviceIndex].FreezePos = controllers[___deviceIndex].PrevPos;
+            controllers[___deviceIndex].FreezeRot = controllers[___deviceIndex].PrevRot;
+            controllers[___deviceIndex].frozen = false;
+        }
+        else if (!controllers[___deviceIndex].frozen) controllers[___deviceIndex].frozen = true;
+
+        controllers[___deviceIndex].PrevPos = controllers[___deviceIndex].transform.position;
+        controllers[___deviceIndex].PrevRot = controllers[___deviceIndex].transform.rotation;
+
+        if (controllers[___deviceIndex].frozen)
+        {
+            controllers[___deviceIndex].transform.position = controllers[___deviceIndex].FreezePos;
+            controllers[___deviceIndex].transform.rotation = controllers[___deviceIndex].FreezeRot;
+        }
+    }
 }
