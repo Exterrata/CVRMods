096b7e35f42d7c036b20615c933072c5fcad2be4
Exterrata <38149279+Exterrata@users.noreply.github.com>
2023-07-30 21:20:38 -0400

    fixed working with TrackedControllerFix

---
+++ ControllerFreeze/ControllerFreezer.cs
@@ -0,0 +1,37 @@
+﻿using ABI_RC.Core.Savior;
+using UnityEngine;
+
+namespace Koneko;
+public class ControllerFreezer : MonoBehaviour
+{
+    public Vector3 PrevPos;
+    public Quaternion PrevRot;
+    public Vector3 FreezePos;
+    public Quaternion FreezeRot;
+    public bool frozen;
+
+    public void LateUpdate()
+    {
+        if (!MetaPort.Instance.isUsingVr || CheckVR.Instance.forceOpenXr || !gameObject.activeInHierarchy) return;
+
+        if (transform.position != PrevPos)
+        {
+            FreezePos = PrevPos;
+            FreezeRot = PrevRot;
+            frozen = false;
+        }
+        else if (!frozen)
+        {
+            frozen = true;
+        }
+
+        PrevPos = transform.position;
+        PrevRot = transform.rotation;
+
+        if (frozen)
+        {
+            transform.position = FreezePos;
+            transform.rotation = FreezeRot;
+        }
+    }
+}
--- ControllerFreeze/Main.cs
+++ ControllerFreeze/Main.cs
@@ -1,20 +1,18 @@
-﻿using System;
+﻿using ABI_RC.Core.Player;
 using MelonLoader;
+using UnityEngine;
 
 [assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
-[assembly: MelonInfo(typeof(Koneko.ControllerFreeze), "ControllerFreeze", "1.1.0", "Exterrata")]
+[assembly: MelonInfo(typeof(Koneko.ControllerFreeze), "ControllerFreeze", "1.2.0", "Exterrata")]
 [assembly: HarmonyDontPatchAll]
 
 namespace Koneko;
 public class ControllerFreeze : MelonMod
 {
-    public override void OnInitializeMelon()
+    public override void OnSceneWasInitialized(int buildIndex, string sceneName)
     {
-        Patches.controllers = new Dictionary<int, Patches.Controller>();
-        try {
-            HarmonyInstance.PatchAll(typeof(Patches));
-        } catch(Exception e) { 
-            MelonLogger.Error(e);
-        }
+        if (buildIndex != 3) return;
+        PlayerSetup.Instance.vrLeftHandTracker.AddComponent(typeof(ControllerFreezer));
+        PlayerSetup.Instance.vrRightHandTracker.AddComponent(typeof(ControllerFreezer));
     }
 }
